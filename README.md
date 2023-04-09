8b2bdb74072f4b91b585bb2559091d716086cfcb
Mathias Gröbe <mathias.groebe@gmail.com>
2023-04-09 21:46:14 +0000

    First draft

--- README.md
+++ README.md
@@ -1,22 +1,133 @@
 # Graubrot
 A osm2pgsql configuration for the flex-backend for everyday use. Imports common objects for make a simple big scale map with no generlization. It tries to clean the attributes to create a easy to handle data model.
 
+## Running
+
+###  Quickstart
+
+1. Create a PostgrSQL Database
+2. Enable PostGIS by executing ```CREATE EXTENSION POSTGIS```
+3. Create a schema for the data import ```CREATE SCHEMA osm```. You can also use another Schema. It is just the default value.
+4. Run ```osm2pgsql -c -O flex -S graubrot.lua -d postgres://USER:PASS@HOST/DB_NAME  YOUR_OSM_FILE.osm.pbf```
+
+### Hints
+
+- Security
+- Index 
+- ...
+
+
 ## Layers and attributes 
 
-### Area
+###  Forest
 
-####  Forest
+Forest layer with name of the objects, by combining the two common classifications.
 
-####  Water
+```mermaid
+graph TD;
+    landuse=forest-->forest;
+    natural=wood-->forest
+```
 
-### Line
+| Attribute | Type | Index | Describtion |
+| :---      | :--- | :---  | :---       |
+| area_id | int | | Id of OSM object, needed for running updates |
+| fid | int | | Feature id |
+| name | text | | Name of the object |
+| name_en | text | | Englisch name of the object | 
+| geom | Multipolygon geometry | GiST | Multipolygon geometry of the OSM object |
 
-#### Traffic
+### Water
 
-#### Waterway
+Water area layer with name of the objects, by combining the two common classifications.
 
-### Point
+```mermaid
+graph TD;
+    natural=water-->water;
+    waterway=riverbank-->water
+```
 
-#### POI
+| Attribute | Type | Index | Describtion |
+| :---      | :--- | :---  | :---        |
+| area_id | int | | Id of OSM object, needed for running updates |
+| fid | int | | Feature id |
+| name | text | | Name of the object |
+| name_en | text | | Englisch name of the object | 
+| geom | Multipolygon geometry | GiST| Multipolygon geometry of the OSM object |
 
-#### Peaks
+### Building
+
+Buildings layer with name, and adress of the objects.
+
+```mermaid
+graph TD;
+    buidling=*-->building;
+    
+```
+
+| Attribute | Type | Index | Describtion |
+| :---      | :--- | :---  | :---        |
+| area_id | int | | Id of OSM object, needed for running updates |
+| fid | int | | Feature id |
+| building | text | B-Tree | Content of the ```building``` tag | 
+| name | text | | Name of the object |
+| name_en | text | | Englisch name of the object | 
+| street | text | | Content of the ```addr:street``` tag | 
+| housenumber | text | | Content of the ```addr:housenumber``` tag | 
+| postcode | text | | Content of the ```addr:postcode``` tag | 
+| city | text | | Content of the ```addr:city``` tag | 
+| geom | Multipolygon geometry | GiST | Multipolygon geometry of the OSM object |
+
+### Traffic
+
+Line layer with roads and railways with name of the objects, and essential other tags in a cleaned way.
+
+```mermaid
+graph TD;
+    highway=*-->traffic;
+    railway=*-->traffic
+```
+
+| Attribute | Type | Index | Describtion |
+| :---      | :--- | :---  | :---        |
+| way_id | int | | Id of OSM object, needed for running updates |
+| fid | int | | Feature id |
+| highway | text | B-Tree | Content of the ```highway``` tag | 
+| railway | text | B-Tree | Content of the ```railway``` tag | 
+| name | text | | Name of the object |
+| name_en | text | | Englisch name of the object | 
+| service | text | | Content of the ```service``` tag | 
+| usage | text | | Content of the ```usage``` tag | 
+| tracktype | text | | Content of the ```tracktype``` tag | 
+| trail_visibility | text | | Content of the ```trail_visibility``` tag |
+| oneway | bool | B-Tree | Content of the ```oneway``` tag converted to a bool | 
+| bridge | bool | B-Tree | Content of the ```bridge``` tag converted to a bool | 
+| tunnel | bool | B-Tree |  Content of the ```tunnel``` tag converted to a bool | 
+| layer | real | B-Tree | Content of the ```layer``` tag converted to a number | 
+| ref | bool | | Content of the ```ref``` tag | 
+| geom | Linestring geometry | GiST | Linestring geometry of the OSM object |
+
+### Waterway
+
+Waterway linstring layer with name of the objects, and common attributes.
+
+```mermaid
+graph TD;
+    waterway=*-->waterway
+```
+
+| Attribute | Type | Index | Describtion |
+| :---      | :--- | :---  | :---        |
+| way_id | int | | Id of OSM object, needed for running updates |
+| fid | int | | Feature id |
+| waterway | text | B-Tree | Content of the ```oneway``` tag |
+| intermittent | bool | B-Tree | Content of the ```intermittent``` tag converted to a bool | 
+| tunnel | bool | B-Tree |  Content of the ```tunnel``` tag converted to a bool | 
+| layer | real | | Content of the ```layer``` tag converted to a number | 
+| name | text | | Name of the object |
+| name_en | text | | Englisch name of the object | 
+| geom | Linestring geometry | GiST| Linestring geometry of the OSM object |
+
+
+
+TODO:: POI + Peak
---
+++ graubrot.lua
@@ -0,0 +1,405 @@
+print('osm2pgsql version: ' .. osm2pgsql.version)
+
+-- Variables
+local tables = {}
+local import_schema = 'osm' -- Defines the import schema
+local epsg_code = 32633 -- Defines the projection
+
+-- Table defenitions
+tables.forest = osm2pgsql.define_table({
+    name = 'forest',
+    schema = import_schema,
+    ids = {
+        type = 'area',
+        id_column = 'area_id'
+    },
+    columns = {{
+        column = 'fid',
+        sql_type = 'serial',
+        create_only = true
+    }, {
+        column = 'name',
+        type = 'text'
+    }, {
+        column = 'name_en',
+        type = 'text'
+    }, {
+        column = 'geom',
+        type = 'multipolygon',
+        projection = epsg_code
+    }}
+})
+
+tables.water = osm2pgsql.define_table({
+    name = 'water',
+    schema = import_schema,
+    ids = {
+        type = 'area',
+        id_column = 'area_id'
+    },
+    columns = {{
+        column = 'fid',
+        sql_type = 'serial',
+        create_only = true
+    }, {
+        column = 'name',
+        type = 'text'
+    }, {
+        column = 'name_en',
+        type = 'text'
+    }, {
+        column = 'geom',
+        type = 'multipolygon',
+        projection = epsg_code
+    }}
+})
+
+tables.building = osm2pgsql.define_table({
+    name = 'building',
+    schema = import_schema,
+    ids = {
+        type = 'area',
+        id_column = 'area_id'
+    },
+    columns = {{
+        column = 'fid',
+        sql_type = 'serial',
+        create_only = true
+    }, {
+        column = 'building',
+        type = 'text'
+    }, {
+        column = 'name',
+        type = 'text'
+    }, {
+        column = 'name_en',
+        type = 'text'
+    }, {
+        column = 'street',
+        type = 'text'
+    }, {
+        column = 'housenumber',
+        type = 'text'
+    }, {
+        column = 'postcode',
+        type = 'text'
+    }, {
+        column = 'city',
+        type = 'text'
+    }, {
+        column = 'geom',
+        type = 'multipolygon',
+        projection = epsg_code
+    }},
+    indexes = {{
+        column = 'building',
+        method = 'btree'
+    }}
+})
+
+tables.traffic = osm2pgsql.define_table({
+    name = 'traffic',
+    schema = import_schema,
+    ids = {
+        type = 'way',
+        id_column = 'way_id'
+    },
+    columns = {{
+        column = 'fid',
+        sql_type = 'serial',
+        create_only = true
+    }, {
+        column = 'highway',
+        type = 'text'
+    }, {
+        column = 'railway',
+        type = 'text'
+    }, {
+        column = 'name',
+        type = 'text'
+    }, {
+        column = 'name_en',
+        type = 'text'
+    }, {
+        column = 'service',
+        type = 'text'
+    }, {
+        column = 'usage',
+        type = 'text'
+    }, {
+        column = 'tracktype',
+        type = 'text'
+    }, {
+        column = 'trail_visibility',
+        type = 'text'
+    }, {
+        column = 'oneway',
+        type = 'bool'
+    }, {
+        column = 'bridge',
+        type = 'bool'
+    }, {
+        column = 'tunnel',
+        type = 'bool'
+    }, {
+        column = 'layer',
+        type = 'real'
+    }, {
+        column = 'ref',
+        type = 'text'
+    }, {
+        column = 'geom',
+        type = 'linestring',
+        projection = epsg_code
+    }},
+    indexes = {{
+        column = 'highway',
+        method = 'btree'
+    }, {
+        column = 'railway',
+        method = 'btree'
+    }, {
+        column = 'oneway',
+        method = 'btree'
+    }, {
+        column = 'bridge',
+        method = 'btree'
+    }, {
+        column = 'tunnel',
+        method = 'btree'
+    }, {
+        column = 'layer',
+        method = 'btree'
+    }}
+})
+
+tables.waterway = osm2pgsql.define_table({
+    name = 'waterway',
+    schema = import_schema,
+    ids = {
+        type = 'way',
+        id_column = 'area_id'
+    },
+    columns = {{
+        column = 'fid',
+        sql_type = 'serial',
+        create_only = true
+    }, {
+        column = 'waterway',
+        type = 'text'
+    }, {
+        column = 'name',
+        type = 'text'
+    }, {
+        column = 'name_en',
+        type = 'text'
+    }, {
+        column = 'intermittent',
+        type = 'bool'
+    }, {
+        column = 'tunnel',
+        type = 'bool'
+    }, {
+        column = 'layer',
+        type = 'real'
+
+    }, {
+        column = 'geom',
+        type = 'linestring',
+        projection = epsg_code
+    }},
+    indexes = {{
+        column = 'waterway',
+        method = 'btree'
+    }, {
+        column = 'intermittent',
+        method = 'btree'
+    }, {
+        column = 'tunnel',
+        method = 'btree'
+
+    }}
+})
+
+-- Helper functions 
+
+local function clean_tunnel(object)
+    -- Make tunnel value a bool
+    local tunnel = nil
+    if object.tags.tunnel == 'yes' then
+        tunnel = true
+    elseif object.tags.tunnel == 'culvert' then
+        tunnel = true
+    elseif object.tags.tunnel == 'building_passage' then
+        tunnel = true
+    elseif object.tags.tunnel == 'flooded' then
+        tunnel = true
+    elseif object.tags.tunnel == 'no' then
+        tunnel = false
+    elseif object.tags.tunnel == 'covered' then
+        tunnel = true
+    elseif object.tags.tunnel == 'avalanche_protector' then
+        tunnel = true
+    elseif object.tags.tunnel == 'passage' then
+        tunnel = true
+    end
+    return tunnel
+end
+
+local function clean_bridge(object)
+    local bridge = nil
+    if object.tags.bridge == 'yes' then
+        bridge = true
+    elseif object.tags.bridge == 'viaduct' then
+        bridge = true
+    elseif object.tags.bridge == 'boardwalk' then
+        bridge = true
+    elseif object.tags.bridge == 'aqueduct' then
+        bridge = true
+    elseif object.tags.bridge == 'no' then
+        bridge = false
+    elseif object.tags.bridge == 'covered' then
+        bridge = true
+    elseif object.tags.bridge == 'cantilever' then
+        bridge = true
+    elseif object.tags.bridge == 'trestle' then
+        bridge = true
+    end
+    return bridge
+end
+
+local function clean_oneway(object)
+    local oneway = false
+    if object.tags.oneway == 'yes' then
+        oneway = true
+    elseif object.tags.oneway == 'no' then
+        oneway = false
+    end
+    return oneway
+end
+
+function str_to_bool(str)
+    if str == nil then
+        return false
+    end
+    return string.lower(str) == 'true'
+end
+
+
+-- Function which fill the tables
+
+function osm2pgsql.process_way(object)
+
+    -- A closed way that also has the right tags for an area is a polygon.
+    if object.is_closed and (object.tags.landuse == 'forest' or object.tags.natural == 'wood') then
+        tables.forest:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+    if object.is_closed and (object.tags.natural == 'water' or object.tags.waterway == 'riverbank') then
+        tables.water:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+    if object.is_closed and object.tags.building then
+        tables.building:add_row({
+            building = object.tags.building,
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            street = object.tags['addr:street'],
+            house_number = object.tags['addr:housenumber'],
+            postcode = object.tags['addr:postcode'],
+            city = object.tags['addr:city'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+    if object.tags.highway or object.tags.railway then
+        tables.traffic:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            highway = object.tags.highway,
+            railway = object.tags.railway,
+            service = object.tags.service,
+            usage = object.tags.usage,
+            tracktype = object.tags.tracktype,
+            oneway = clean_oneway(object), -- make it a bool
+            bridge = clean_bridge(object), -- make it a bool
+            tunnel = clean_tunnel(object), -- make it a bool
+            layer = tonumber(object.tags.layer), -- convert it to a number
+            ref = object.tags.ref,
+            geom = {
+                create = 'line'
+            }
+        })
+    end
+
+    if object.tags.waterway then
+        tables.waterway:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            waterway = object.tags.waterway,
+            tunnel = clean_tunnel(object),
+            layer = tonumber(object.tags.layer),
+            intermittent = str_to_bool(object.tags.intermittent),
+            geom = {
+                create = 'line'
+            }
+        })
+    end
+
+end
+
+function osm2pgsql.process_relation(object)
+
+    local type = object:grab_tag('type')
+
+    -- Store multipolygon relations as polygons
+    if type == 'multipolygon' and (object.tags.landuse == 'forest' or object.tags.natural == 'forest') then
+        tables.forest:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+    if type == 'multipolygon' and (object.tags.natural == 'water' or object.tags.waterway == 'riverbank') then
+        tables.water:add_row({
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+    if type == 'multipolygon' and object.tags.building then
+        tables.building:add_row({
+            building = object.tags.building,
+            name = object.tags.name,
+            name_en = object.tags['name:en'],
+            street = object.tags['addr:street'],
+            house_number = object.tags['addr:housenumber'],
+            postcode = object.tags['addr:postcode'],
+            city = object.tags['addr:city'],
+            geom = {
+                create = 'area'
+            }
+        })
+    end
+
+end
