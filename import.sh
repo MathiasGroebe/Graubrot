cfe752118d1ce29fdd08e437326837aa49cb8fcb
Mathias Gröbe <mathias.groebe@wheregroup.com>
2023-10-21 11:15:38 +0200

    Example in Sachsen

--- import.sh
+++ import.sh
@@ -2,4 +2,4 @@
 
 export PGSERVICE=graubrot
 
-osm2pgsql -O flex -S graubrot.lua berlin-latest.osm.pbf
+osm2pgsql -O flex -S graubrot.lua -b 13.576881,50.77216,13.842782,50.911776  sachsen-latest.osm.pbf
