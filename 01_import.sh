08ed972365938f52e5c74c9bcbd209e4658d3dea
Mathias Gröbe <mathias.groebe@wheregroup.com>
2024-02-03 14:09:49 +0100

    Better import script

--- 01_import.sh
+++ 01_import.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export PGSERVICE=graubrot
+bbox="13.662356,50.792525,13.888777,50.89853" 
 
-osmium extract -b 13.662356,50.792525,13.888777,50.89853 -o region.osm.pbf sachsen-latest.osm.pbf 
+echo "Clip to bounding box..."
+osmium extract -b $bbox --overwrite -o region.osm.pbf sachsen-latest.osm.pbf 
+echo "Import into database..."
 osm2pgsql -O flex -S graubrot.lua region.osm.pbf 
