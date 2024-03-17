70d1eca1f7c8a8557aeeb585c580f64b98418ee1
Mathias Gröbe <mathias.groebe@wheregroup.com>
2024-03-17 21:02:17 +0100

    Variables

--- 01_import.sh
+++ 01_import.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-export PGSERVICE=graubrot
-bbox="13.662356,50.792525,13.888777,50.89853" 
+# name of pg service, which should be used to access the database
+pg_service="graubrot"
+# xmin, ymin, xmax, ymax
+bbox="13.7479,50.8341,13.8107,50.8629"
+# raw OSM file (PBF or OSM-XML) 
+input_osm="sachsen-latest.osm.pbf"
+
+export PGSERVICE=$pg_service
 
 echo "Clip to bounding box..."
-osmium extract -b $bbox --overwrite -o region.osm.pbf sachsen-latest.osm.pbf 
+osmium extract -b $bbox --overwrite -o region.osm.pbf $input_osm
 echo "Import into database..."
 osm2pgsql -O flex -S graubrot.lua region.osm.pbf 
