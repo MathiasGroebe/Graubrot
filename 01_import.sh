61fcbf0c64b1203f6879f04a5ad407260ab75aa4
Mathias Gröbe <mathias.groebe@wheregroup.com>
2025-11-26 13:18:51 +0100

    Smart cut

--- 01_import.sh
+++ 01_import.sh
@@ -3,13 +3,13 @@
 # name of pg service, which should be used to access the database
 pg_service="graubrot"
 # xmin, ymin, xmax, ymax
-bbox="13.7479,50.8341,13.8107,50.8629"
+bbox="13.493184,50.911937,13.655232,51.000389" #Tharandt
 # raw OSM file (PBF or OSM-XML) 
 input_osm="sachsen-latest.osm.pbf"
 
 export PGSERVICE=$pg_service
 
 echo "Clip to bounding box..."
-osmium extract -b $bbox --overwrite -o region.osm.pbf $input_osm
+osmium extract -b $bbox -s smart --overwrite -o region.osm.pbf $input_osm
 echo "Import into database..."
 osm2pgsql -O flex -S graubrot.lua region.osm.pbf 
