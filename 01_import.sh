970b55e452f71010101dc4da0656968762242f37
Mathias Gröbe <mathias.groebe@gmail.com>
2026-05-07 23:22:43 +0200

    Adjust import

--- 01_import.sh
+++ 01_import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # name of pg service, which should be used to access the database
-pg_service="tmp"
+pg_service="graubrot"
 schema="osm"
 # xmin, ymin, xmax, ymax
 bbox="11.8723,50.1713,15.0419,51.6851" 
@@ -10,20 +10,20 @@ input_osm="sachsen-latest.osm.pbf"
 
 export PGSERVICE=$pg_service
 
-# echo "Clip to bounding box..."
-# osmium extract -b $bbox -s smart --overwrite -o region.osm.pbf $input_osm
+echo "Clip to bounding box..."
+osmium extract -b $bbox -s smart --overwrite -o region.osm.pbf $input_osm
 
-# echo "Import into database..."
-# osm2pgsql -O flex -S graubrot.lua region.osm.pbf 
+echo "Import into database..."
+osm2pgsql -O flex -S graubrot.lua region.osm.pbf 
 
 echo "Importing land and water polygons"
 ogr2ogr PG:service=$pg_service -lco FID=fid -lco GEOMETRY_NAME=geom land-polygons-split-4326/land_polygons.shp -nln $schema.land -nlt MULTIPOLYGON -overwrite 
 ogr2ogr PG:service=$pg_service -lco FID=fid -lco GEOMETRY_NAME=geom water-polygons-split-4326/water_polygons.shp -nln $schema.ocean -nlt MULTIPOLYGON -overwrite
 
-# echo "Creating primary keys..."
-# psql -f 10_create_pk.sql
+echo "Creating primary keys..."
+psql -f 10_create_pk.sql
 
-# echo  "Calculating isolation..."
-# psql -f 10_isolation.sql
+echo  "Calculating isolation..."
+psql -f 10_isolation.sql
 
 echo "Import finished"
